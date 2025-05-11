d79993cbed496c81ae7c3f60bfde8b796ca4f23a
shershenARP <mikhashtan@gmail.com>
2025-05-11 12:32:57 +0300

    Update RoleManager.java
    заходят как-то в бар клоун, амогус и рпшер...

--- core/src/mindustry/ui/fragments/RoleManager.java
+++ core/src/mindustry/ui/fragments/RoleManager.java
@@ -3,22 +3,31 @@ package mindustry.ui.fragments;
 import mindustry.gen.Player;
 import arc.struct.ObjectMap;
 import arc.graphics.Color;
+import mindustry.graphics.Pal;
 
 public class RoleManager {
     private static final ObjectMap<Player, String> roles = new ObjectMap<>();
 
     public static void setRole(Player player, String role) {
-        roles.put(player, role);
+        if (role == "Core") {
+            role = "\uE853";
+            roles.put(player, role);
+        };
+
+        if (role == "Defense") {
+            role = "\uE84D";
+            roles.put(player, role);
+        };
     }
 
     public static String getRole(Player player) {
-        return roles.get(player, "No Role");
+        return roles.get(player, "404");
     }
 
     public static Color roleColorFor(String role) {
         switch(role) {
-            case "Core": return Color.yellow;
-            case "Defense": return Color.orange;
+            case "\uE853": return Pal.stat;
+            case "\uE84D": return Pal.unitBack;
             default: return Color.gray;
         }
     }
