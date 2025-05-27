e9d7b4919ed7cbaa5c98fa58b0b7973c5a31d810
shershenARP <mikhashtan@gmail.com>
2025-05-27 21:38:28 +0300

    Я что-то хотел из него удалить, но забыл что.
    - Добавлен окт и зенитх.
    - Решил нагло позаимствовать прикольную датабазу у анюкена.
    - Не знаю, что тут ещё можно добавить.

--- core/src/mindustry/ui/dialogs/DatabaseDialog.java
+++ core/src/mindustry/ui/dialogs/DatabaseDialog.java
@@ -5,11 +5,13 @@ import arc.graphics.*;
 import arc.input.*;
 import arc.math.*;
 import arc.scene.event.*;
+import arc.scene.style.*;
 import arc.scene.ui.*;
 import arc.scene.ui.layout.*;
 import arc.struct.*;
 import arc.util.*;
 import mindustry.*;
+import mindustry.content.*;
 import mindustry.ctype.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
@@ -24,16 +26,30 @@ public class DatabaseDialog extends BaseDialog{
     private TextField search;
     private Table all = new Table();
 
+    private @Nullable Seq<UnlockableContent> allTabs;
+    //sun means "all content"
+    private UnlockableContent tab = Planets.sun;
+
     public DatabaseDialog(){
         super("@database");
 
         shouldPause = true;
         addCloseButton();
-        shown(this::rebuild);
+        shown(() -> {
+            checkTabList();
+            if(state.isCampaign() && allTabs.contains(state.getPlanet())){
+                tab = state.getPlanet();
+            }else if(state.isGame() && state.rules.planet != null && allTabs.contains(state.rules.planet)){
+                tab = state.rules.planet;
+            }
+
+            rebuild();
+        });
         onResize(this::rebuild);
 
-        all.margin(20).marginTop(0f);
+        all.margin(20).marginTop(0f).marginRight(30f);
 
+        cont.top();
         cont.table(s -> {
             s.image(Icon.zoom).padRight(8);
             search = s.field(null, text -> rebuild()).growX().get();
@@ -43,18 +59,51 @@ public class DatabaseDialog extends BaseDialog{
         cont.pane(all).scrollX(false);
     }
 
+    void checkTabList(){
+        if(allTabs == null){
+            Seq<Content>[] allContent = Vars.content.getContentMap();
+            ObjectSet<UnlockableContent> all = new ObjectSet<>();
+            for(var contents : allContent){
+                for(var content : contents){
+                    if(content instanceof UnlockableContent u) {
+                        all.addAll(u.databaseTabs);
+                    }
+                }
+            }
+            allTabs = all.toSeq().sort();
+            allTabs.insert(0, Planets.sun);
+        }
+    }
+
     void rebuild(){
+        checkTabList();
+
         all.clear();
-        var text = search.getText();
+        var text = search.getText().toLowerCase();
 
         Seq<Content>[] allContent = Vars.content.getContentMap();
+
+        all.table(t -> {
+            int i = 0;
+            for(var content : allTabs){
+                t.button(content == Planets.sun ? Icon.eyeSmall : content instanceof Planet p ? Icon.icons.get(p.icon, Icon.commandRally) : new TextureRegionDrawable(content.uiIcon), Styles.clearNoneTogglei, iconMed, () -> {
+                    tab = content;
+                    rebuild();
+                }).size(50f).checked(b -> tab == content).tooltip(content == Planets.sun ? "@all" : content.localizedName).with(but -> {
+                    but.getStyle().imageUpColor = content instanceof Planet p ? p.iconColor : Color.white.cpy();
+                });
+
+                if(++i % 10 == 0) t.row();
+            }
+        }).row();;
 
         for(int j = 0; j < allContent.length; j++){
             ContentType type = ContentType.all[j];
 
             Seq<UnlockableContent> array = allContent[j]
-                .select(c -> c instanceof UnlockableContent u && !u.isHidden()  &&
-                    (text.isEmpty() || u.localizedName.toLowerCase().contains(text.toLowerCase()))).as();
+                    .select(c -> c instanceof UnlockableContent u && !u.isHidden() && !u.hideDatabase && (tab == Planets.sun || u.allDatabaseTabs || u.databaseTabs.contains(tab)) &&
+                            (text.isEmpty() || u.localizedName.toLowerCase().contains(text))).as();
+
             if(array.size == 0) continue;
 
             all.add("@content." + type.name() + ".name").growX().left().color(Pal.accent);
@@ -70,7 +119,7 @@ public class DatabaseDialog extends BaseDialog{
                 for(int i = 0; i < array.size; i++){
                     UnlockableContent unlock = array.get(i);
 
-                    Image image = unlocked(unlock) || state.isMenu() ? new Image(unlock.uiIcon).setScaling(Scaling.fit) : new Image(Icon.lock, Pal.gray);
+                    Image image = unlocked(unlock) ? new Image(new TextureRegionDrawable(unlock.uiIcon), mobile ? Color.white : Color.lightGray).setScaling(Scaling.fit) : new Image(Icon.lock, Pal.gray);
 
                     //banned cross
                     if(state.isGame() && (unlock instanceof UnitType u && u.isBanned() || unlock instanceof Block b && state.rules.isBanned(b))){
@@ -84,12 +133,12 @@ public class DatabaseDialog extends BaseDialog{
 
                     ClickListener listener = new ClickListener();
                     image.addListener(listener);
-                    if(!mobile && (unlocked(unlock) || state.isMenu())){
+                    if(!mobile && unlocked(unlock)){
                         image.addListener(new HandCursorListener());
                         image.update(() -> image.color.lerp(!listener.isOver() ? Color.lightGray : Color.white, Mathf.clamp(0.4f * Time.delta)));
                     }
 
-                    if(unlocked(unlock) || state.isMenu()){
+                    if(unlocked(unlock)){
                         image.clicked(() -> {
                             if(Core.input.keyDown(KeyCode.shiftLeft) && Fonts.getUnicode(unlock.name) != 0){
                                 Core.app.setClipboardText((char)Fonts.getUnicode(unlock.name) + "");
@@ -115,6 +164,6 @@ public class DatabaseDialog extends BaseDialog{
     }
 
     boolean unlocked(UnlockableContent content){
-        return (!Vars.state.isCampaign() && !Vars.state.isMenu()) || content.unlocked();
+        return (!Vars.state.isCampaign()) || content.unlocked();
     }
 }
